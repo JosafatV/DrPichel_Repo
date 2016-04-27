a4d9235f29ed4b56ed2537dda35c69af0b1e6be9
JosafatV <josav09@gmail.com>
2016-04-26 22:32:53 -0600

    SP Cobro Doctores
    Stored Procedures - Cobro a doctores

--- Scripts SQL/SP Cobro Doctores.sql
+++ Scripts SQL/SP Cobro Doctores.sql
@@ -4,7 +4,8 @@ GO
 CREATE PROCEDURE SP_Cobro_Doctores 
 		@MONTO INT, @FromDate DATE, @ToDate DATE
 	AS
-		SELECT D.Cedula, D.Nombre, D.Apellido, COUNT(Pd.IdPaciente) AS NoPacientes, COUNT(Pd.IdPaciente)*@MONTO AS MONTO
-		FROM DOCTOR AS D, PACIENTE_POR_DOCTOR AS Pd, CITA AS C
-		WHERE D.NoDoctor=Pd.NoDoctor AND C.IdPaciente=Pd.IdPaciente AND C.Fecha BETWEEN @FromDate AND @ToDate
+		SELECT U.Nombre, U.Apellido, COUNT(Cd.NoCita) AS Cantidad_De_Citas, COUNT(Cd.NoCita)*@MONTO AS Cobro
+		FROM USUARIO AS U, DOCTOR AS D, CITA_POR_DOCTOR AS Cd
+		WHERE U.Id = D.UserId AND  U.Estado='A' AND D.NoDoctor=Cd.NoDoctor
+		GROUP BY U.Nombre, U.Apellido
 		
