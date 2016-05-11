ad5dcd2965d2665795a22d51262343c26a141a8d
Giovanni Villalobos <gjosue253@gmail.com>
2016-05-11 00:24:37 -0600

    Se le agregarion cambios a cobros

--- DrPhischelWebApp/DrPhischelWebApp/Scripts/WebAppScripts/AdminViewControllers/cobrosController.js
+++ DrPhischelWebApp/DrPhischelWebApp/Scripts/WebAppScripts/AdminViewControllers/cobrosController.js
@@ -1,5 +1,12 @@
-angular.module('DrPhischelApp').controller('cobrosController', ['$scope', '$routeParams',
-    '$location', function($scope,$routeParams,$location){
-    //alert('Estoy en cobros controller')
+angular.module('DrPhischelApp').controller('cobrosController', ['$scope', '$routeParams','drPhischelApiResource',
+    '$location', function ($scope, $routeParams, $location, drPhischelApiResource) {
+
+        $scope.verCobros = function () {
+            drPhischelApiResource.query({ type: 'Doctor', extension: 'Cobros', extension2: $scope.dateCobro }).$promise.then(function (data) {
+                $scope.docsAlCobro = data;
+                alert(data);
+            })
+        };
+
     
     }]);
